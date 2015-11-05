d6bb2582fc64693fb653f04a55cbbfb5ed2f2e09
click20 <rocco.davide@icloud.com>
2015-11-05 10:29:24 +0100

    esercizio

--- app.js
+++ app.js
@@ -8,14 +8,36 @@
  *    phrase_de : "Ich bin ein Mann"  // sentence in german
  *  }
  */ 
+var index=0;
+var score=0 ;
 
 var tmpl = ' <li id="ID">' +
            '  <h3>SENTENCE</h3>' +
            ' </li> ';
 
 $(document).ready(function(){
-
-  
+    
+    
+    $(".sentences").append(tmpl.replace("SENTENCE",data[0].phrase_en) ).addClass("current");
+    index++;
+    
+    $(".btn opt-continue").click( function(event){
+        index++;
+        
+       for(s=0; s<index; s++){
+        $(".sentences li").removeClass("current");
+       }
+        
+        $(".sentences").append(tmpl.replace("SENTENCE",data[index++].phrase_en) ); 
+        
+     if( $(".sencetences").text() == data[index].phrase_de){
+         score++;
+     }  
+        
+    } )
+    
+    
+    
 });
 
 
